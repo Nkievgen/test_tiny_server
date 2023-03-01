ea50ff2334a2a9dcf1819a79e4a1aefe406a58d8
Nkievgen <nkievgen@gmail.com>
2023-03-01 13:32:32 +0200

    separated imports in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const {register, login, list, assign} = require('./controller');
 const error = require('./error-handler');
 const sequelize = require('./database');
